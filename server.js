b1d74509b0a011443e2ac0a02af6bde1c4880c34
Samr Mouna <samr.mouna@gmail.com>
2023-07-12 15:40:38 -0400

    created post function, called model authenticator

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const { sequelize } = require("./models");
+const { sequelize, Recipe } = require("./models");
 const express = require("express");
 
 const app = express();
 
-app.post("/recipes", async (req, res) => {
 app.use(express.json());
+
+app.post("/recipes", async (req, res) => {
+    try {
+        const recipe = await Recipe.create(req.body)
+        return res.json(recipe);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(err);
+    }
+})
+
+app.listen({ port: 4000 }, async () => {
+    console.log(`Server running at http://localhost:4000`);
+    await sequelize.authenticate();
+    console.log("running...");
 })
