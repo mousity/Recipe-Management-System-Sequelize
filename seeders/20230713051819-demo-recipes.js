17fa91c21b3e6ac1e53c5ebce03289de6f6d4e99
Samr Mouna <samr.mouna@gmail.com>
2023-07-13 03:25:11 -0400

    added minimal comments

--- seeders/20230713051819-demo-recipes.js
+++ seeders/20230713051819-demo-recipes.js
@@ -1,8 +1,9 @@
 'use strict';
-
+// Playing around with unique IDs
 const { v4: uuidv4 } = require('uuid');
 const { UUID, UUIDV4 } = require('sequelize');
 
+// Seeder to intiialize database with set data
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -17,7 +18,7 @@ module.exports = {
     }, {
       uuid: uuidv4(),
       title: "Fried Eggs",
-      description: "Savory eggs to heal the soul. Serves 4. One egg per person for 3, and the 4th is there to simply admire your cooking.",
+      description: "Savory eggs to heal the soul. Serves 4. One egg per person for 3, and the 4th is simply there to admire your cooking.",
       ingredients: "3 Large Eggs, 1/2 Tbsp of butter, Salt",
       instructions: "Melt butter onto pan. Crack all 3 eggs in pan, and cover. Wait for eggs to finish cooking. Put an egg on each plate and salt heavily. Very heavily. A very large coat of salt.",
       created_at: new Date(),
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 4000;
 app.use(express.json());
 
+// Copied from job_app_tracker to make errors and requests easier to see
 app.use((req, res, next) => {
     console.log(`Request: ${req.method} ${req.originalUrl}`);
     res.on("finish", () => {
@@ -14,30 +15,33 @@ app.use((req, res, next) => {
     next();
   });
 
+// Get function
 app.get("/recipes", async (req, res) => {
     try {
-        const recipes = await Recipe.findAll();
-
+        const recipes = await Recipe.findAll(); // Find ALL rows of data within this model
         return res.json(recipes);
     } catch (err) {
         return res.status(404).send({ message: "Recipes not found"});
     }
 })
 
+// Gets a specific recipe
 app.get("/recipes/:id", async (req, res) => {
     const recipeId = parseInt(req.params.id, 10);
     try {
-        const recipe = await Recipe.findOne({ where: {id: recipeId }});
+        const recipe = await Recipe.findOne({ where: {id: recipeId }}); // Find one record where our ID matches the parameter we parsed
         return res.status(200).json(recipe);
     } catch (err) {
         return res.status(404).send({ message: "Recipe not found"});
     }
 })
 
+// Patch function
 app.patch("/recipes/:id", async (req, res) => {
     const recipeId = parseInt(req.params.id, 10);
   
     try {
+        // Update records where the id matches our parameter with our request body
       const [numberOfAffectedRows, affectedRows] = await JobApplication.update(req.body, { where: { id: recipeId }, returning: true });
   
       if (numberOfAffectedRows > 0) {
@@ -51,9 +55,11 @@ app.patch("/recipes/:id", async (req, res) => {
     }
   });
 
+// Delete function
 app.delete("/recipes/:id", async (req, res) => {
     const recipeId = parseInt(req.params.id, 10);
     try {
+        // Destroy a recipe specified by the id
         const recipe = await Recipe.destroy({ where: {id: recipeId} });
         console.log("Record deleted");
         return res.status(200).json(recipe);
@@ -62,8 +68,10 @@ app.delete("/recipes/:id", async (req, res) => {
     }
 })
 
+// Post function
 app.post("/recipes", async (req, res) => {
     try {
+        // Create a recipe, IDs are generated automatically
         const recipe = await Recipe.create(req.body)
         return res.json(recipe);
     } catch (err) {
@@ -72,10 +80,13 @@ app.post("/recipes", async (req, res) => {
     }
 })
 
+// Put function
 app.put("/recipes/:id", async (req, res) => {
     const recipeId = parseInt(req.params.id, 10);
     try {
+        // Find a recipe with our matched ID
         const recipe = await Recipe.findOne({where: {id: recipeId}});
+        // Replace the recipe with our updated recipe
         await recipe.update(req.body);
 
         return res.json(recipe);
@@ -88,6 +99,7 @@ app.put("/recipes/:id", async (req, res) => {
 
 app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
+    // Safer to use than sync() while still performing the same function
     await sequelize.authenticate();
     console.log("running...");
 })
